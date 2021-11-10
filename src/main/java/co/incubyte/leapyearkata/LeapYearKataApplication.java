726ff26c7bcaf0c3aa5440b17a3d4451e9dc5cab
Dinesh-Girbide <ddgirbide@gmail.com>
2021-11-10 17:03:25 +0530

    added spring starter project

---
+++ src/main/java/co/incubyte/leapyearkata/LeapYearKataApplication.java
@@ -0,0 +1,13 @@
+package co.incubyte.leapyearkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapYearKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapYearKataApplication.class, args);
+	}
+
+}
