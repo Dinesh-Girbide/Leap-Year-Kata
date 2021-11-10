61dea9a46f77eac2b7ce3d354926a44b8bfa890b
Dinesh-Girbide <ddgirbide@gmail.com>
2021-11-10 18:20:55 +0530

    Co-authored-by: {Dinesh & Aslam} ENH : Second Test for year divisible by 4 & 100 and not by 400

--- src/main/java/co/incubyte/leapyearkata/LeapYear.java
+++ src/main/java/co/incubyte/leapyearkata/LeapYear.java
@@ -3,8 +3,9 @@ package co.incubyte.leapyearkata;
 public class LeapYear {
 
   public static boolean checkLeapYear(int year) {
-    if(year%4==0)
+    if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)) {
       return true;
+    }
     return false;
   }
 }
--- src/test/java/co/incubyte/leapyearkata/LeapYearTest.java
+++ src/test/java/co/incubyte/leapyearkata/LeapYearTest.java
@@ -1,12 +1,19 @@
 package co.incubyte.leapyearkata;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 class LeapYearTest {
 
   @Test
-  void year_divisible_by_4() {
-    Assertions.assertTrue(LeapYear.checkLeapYear(8));
+  void year_divisible_by_four_should_return_true() {
+    assertTrue(LeapYear.checkLeapYear(8));
+  }
+
+  @Test
+  void year_divisible_by_four_and_onehundred_and_not_by_fourhundred_should_return_false() {
+    assertFalse(LeapYear.checkLeapYear(1800));
   }
 }
